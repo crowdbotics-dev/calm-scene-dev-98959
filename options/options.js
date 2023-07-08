cfa427cc21695672c5b1b941441ea730062c9bd9
Crowdbotics <engineering@crowdbotics.com>
2023-07-08 00:47:52 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "calm-scene-dev-98959",
+  url: "https://calm-scene-dev-98959.botics.co",
+  api: "https://calm-scene-dev-98959.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
